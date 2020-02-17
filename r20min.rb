4f5869c779c18cb9ed68ce083a04c756375ad157
Peter Marets <petermarets@Mbook.local>
2020-02-17 12:25:25 -0500

    Day One Training Completed

--- r20min.rb
+++ r20min.rb
@@ -1,34 +1,34 @@
 class MegaGreeter
-  attr_accessor :names
+  attr_accessor :m_names
 
   # Create the object
   def initialize(names = "World")
-    @names = names
+    m_names = names
   end
 
   # Say hi to everybody
   def say_hi
-    if @names.nil?
+    if m_names.nil?
       puts "..."
-    elsif @names.respond_to?("each")
-      # @names is a list of some kind, iterate!
-      @names.each do |name|
+    elsif m_names.respond_to?("each")
+      # m_names is a list of some kind, iterate!
+      m_names.each do |name|
         puts "Hello #{name}!"
       end
     else
-      puts "Hello #{@names}!"
+      puts "Hello #{m_names}!"
     end
   end
 
   # Say bye to everybody
   def say_bye
-    if @names.nil?
+    if m_names.nil?
       puts "..."
-    elsif @names.respond_to?("join")
+    elsif m_names.respond_to?("join")
       # Join the list elements with commas
-      puts "Goodbye #{@names.join(", ")}.  Come back soon!"
+      puts "Goodbye #{m_names.join(", ")}.  Come back soon!"
     else
-      puts "Goodbye #{@names}.  Come back soon!"
+      puts "Goodbye #{m_names}.  Come back soon!"
     end
   end
 end
@@ -40,18 +40,18 @@ if __FILE__ == $0
   mg.say_bye
 
   # Change name to be "Zeke"
-  mg.names = "Zeke"
+  mg.m_names = "Zeke"
   mg.say_hi
   mg.say_bye
 
   # Change the name to an array of names
-  mg.names = ["Albert", "Brenda", "Charles",
+  mg.m_names = ["Albert", "Brenda", "Charles",
               "Dave", "Engelbert"]
   mg.say_hi
   mg.say_bye
 
   # Change to nil
-  mg.names = nil
+  mg.m_names = nil
   mg.say_hi
   mg.say_bye
 end
